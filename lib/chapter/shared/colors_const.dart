3fb7ea1dd08b35581d3bc5a3797e0bf049bbcfb9
Nitin <nsheoran2005@gmail.com>
2023-10-31 09:38:07 +0530

    ColorsConst class created

---
+++ lib/chapter/shared/colors_const.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class ColorsConst {
+
+  static const Color blueColor = Colors.blue;
+  static const Color whiteColor = Colors.white;
+  static const Color black12Color = Colors.black12;
+
+}

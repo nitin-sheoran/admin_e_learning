5df8b89bfc30b35f71cb7df1acd6994106bdc388
Nitin <nsheoran2005@gmail.com>
2023-10-31 14:22:26 +0530

    ColorsConst color add

--- lib/chapter/shared/colors_const.dart
+++ lib/chapter/shared/colors_const.dart
@@ -5,5 +5,6 @@ class ColorsConst {
   static const Color blueColor = Colors.blue;
   static const Color whiteColor = Colors.white;
   static const Color black12Color = Colors.black12;
+  static const Color redColor = Colors.red;
 
 }

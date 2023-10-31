fbed4a8f05535d793ab8aac69faa3c619889fe96
Nitin <nsheoran2005@gmail.com>
2023-10-31 09:34:31 +0530

    appConst class create

---
+++ lib/course/shared/app_const.dart
@@ -0,0 +1,19 @@
+class AppConst {
+
+  static const String courseName = 'CourseName';
+  static const String imgUrl = 'ImgUrl';
+  static const String courseId = 'courseId';
+
+  static const String titleText = 'Add Course';
+
+  static const String invertedComa = '\'\'';
+
+  static const String courseNameText = 'Course Name';
+  static const String imgUrlText = 'ImgURL';
+
+  static const String buttonText = 'Add Course';
+  static const String buttonText2 = 'Show Course';
+
+
+}
+

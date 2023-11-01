ad4a3e973297746cf65921db4f384a455a4f263b
Nitin <nsheoran2005@gmail.com>
2023-11-01 15:54:11 +0530

    StringConst class create

---
+++ lib/quiz/shared/string_const_class.dart
@@ -0,0 +1,15 @@
+class StringConstClass {
+
+  static const String chapterId = 'chapterId';
+  static const String questionId = 'questionId';
+  static const String question = 'question';
+  static const String option1 = 'option1';
+  static const String option2 = 'option2';
+  static const String option3 = 'option3';
+  static const String option4 = 'option4';
+  static const String correctOption = 'correctOption';
+
+  static const String addQuiz = 'Add Quiz';
+  static const String showQuiz = 'Show Quiz';
+
+}

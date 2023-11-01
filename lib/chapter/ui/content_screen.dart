4d85a19027a6beb2f14e58ad5804e017e5010f59
Nitin <nsheoran2005@gmail.com>
2023-11-01 09:21:55 +0530

    ContentScreen title change

--- lib/chapter/ui/content_screen.dart
+++ lib/chapter/ui/content_screen.dart
@@ -1,5 +1,6 @@
 import 'package:admin_e_learning/chapter/model/chapter_model.dart';
 import 'package:admin_e_learning/chapter/shared/colors_const.dart';
+import 'package:admin_e_learning/quiz/ui/add_question_screen.dart';
 import 'package:flutter/material.dart';
 
 class ContentScreen extends StatefulWidget {
@@ -16,12 +17,6 @@ class _ContentScreenState extends State<ContentScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(
-          widget.chapter.chapterName,
-          style: const TextStyle(
-            color: ColorsConst.whiteColor,
-          ),
-        ),
         leading: IconButton(
           onPressed: () {
             Navigator.pop(context);
@@ -31,12 +26,41 @@ class _ContentScreenState extends State<ContentScreen> {
             color: ColorsConst.whiteColor,
           ),
         ),
+        actions: [
+          Padding(
+            padding: const EdgeInsets.only(right: 10),
+            child: IconButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => const AddQuestionScreen(),
+                  ),
+                );
+              },
+              icon: const Icon(
+                Icons.quiz,
+                color: ColorsConst.whiteColor,
+              ),
+            ),
+          ),
+        ],
         backgroundColor: ColorsConst.blueColor,
       ),
       body: Padding(
         padding: const EdgeInsets.only(left: 20, right: 20, top: 20),
         child: Column(
           children: [
+            Center(
+              child: Text(
+                widget.chapter.chapterName,
+                style:
+                    const TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+              ),
+            ),
+            const SizedBox(
+              height: 40,
+            ),
             Text(
               widget.chapter.content,
               style: const TextStyle(
